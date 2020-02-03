bd9054db1856b0edba8dfd237f46e91f23724db9
ir2010 <46022116+ir2010@users.noreply.github.com>
2020-02-04 01:04:02 +0530

    Splashscreen added

---
+++ app/src/main/java/com/ir_sj/chat/Splashscreen.java
@@ -0,0 +1,33 @@
+package com.ir_sj.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+public class Splashscreen extends AppCompatActivity {
+
+    Handler handle;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        progressBar=findViewById(R.id.pbar);
+        progressBar.getProgress();
+        handle=new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splashscreen.this,MainActivity.class));
+                finish();
+            }
+        },1000);
+    }
+}
+
